e9d27834cbf9ceb489963f8820dff2cc44f8eb33
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-03-18 02:22:17 +0300

    feat: placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="h-screen bg-neutral-900 flex justify-center items-center">
       <h1 className="text-3xl text-slate-300 drop-shadow-[5px_5px_10px_rgba(59,126,238,0.66)]">
-        Next.JS 13 with Tailwind, Prettier. Eslint and TypeScript
+        Under construction, please check back later!
       </h1>
     </main>
   );
