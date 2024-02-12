4063869a871bdf000cceba1df3fd0f393a5807ab
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-02-12 18:42:36 +0300

    feat: default dark mode for MVP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='dark'>
       <body
         className={cn(
           'h-screen w-full antialiased font-sans',
