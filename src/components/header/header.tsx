edfd908a749333f6640b4ef95ef1003328e7ff46
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-02-13 15:22:31 +0300

    feat: limited header width on desktop

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/header/navbar';
 
 const Header = () => {
   return (
-    <header className='mx-2 p-4 rounded-full sticky top-[0.8rem] backdrop-blur-lg shadow-lg bg-accent/70'>
+    <header className='max-w-screen-md mx-2 md:mx-auto p-4 rounded-full sticky top-[0.8rem] backdrop-blur-lg shadow-lg bg-accent/70'>
       <Navbar />
     </header>
   );
